b0ed89a9643eb6a69764b8e47e2e0e73878a68df
Ali_Altarouty <tarooti14dev@gmail.com>
2024-07-31 11:18:18 +0300

    Add new function

--- bin/lab1_git.dart
+++ bin/lab1_git.dart
@@ -1,5 +1,7 @@
-import 'package:lab1_git/lab1_git.dart' as lab1_git;
+void main() {
+  myfunction(10);
+}
 
-void main(List<String> arguments) {
-  print('Hello world: ${lab1_git.calculate()}!');
+myfunction(int num) {
+  print("Hello , Your number is $num");
 }
--- test/lab1_git_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:lab1_git/lab1_git.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
