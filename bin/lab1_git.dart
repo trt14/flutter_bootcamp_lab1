79383588daf41fd086573b1b6f04c48e29860d5e
Ali_Altarouty <tarooti14dev@gmail.com>
2024-07-31 10:50:58 +0300

    add files

---
+++ bin/lab1_git.dart
@@ -0,0 +1,5 @@
+import 'package:lab1_git/lab1_git.dart' as lab1_git;
+
+void main(List<String> arguments) {
+  print('Hello world: ${lab1_git.calculate()}!');
+}
---
+++ test/lab1_git_test.dart
@@ -0,0 +1,8 @@
+import 'package:lab1_git/lab1_git.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
